4e7c6d8bc8313adfe0540eda76c1638e7eb7fc11
bhanu1999 <sakinalabhanu99@gmail.com>
2022-05-18 20:36:23 +0530

    setting up navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import NavBar from './components/NavBar';
 
 export default class App extends Component {
   a='bhanu'
   render() {
-    return (
-      <div>HI THERE {this.a}</div>
+    return (<>
+    
+    <NavBar/>
+    </>
     )
   }
 }
