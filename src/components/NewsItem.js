c8fcb844dfb6c8e7bb47bcbefdac28398c220102
bhanu1999 <sakinalabhanu99@gmail.com>
2022-05-21 00:50:21 +0530

    loading and looping articles array

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-     let {title,description}=this.props;
+     let {title,description,imageUrl}=this.props;
     return (
         <div className="card" style={{width: "18rem"}} >
-        <img src="1.jpg" className="card-img-top" alt="Img Not Found"/>
+        <img src={imageUrl} className="card-img-top" alt="Img Not Found"/>
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <p className="card-text">{description}</p>
-          <a href="/" className="btn btn-primary">Know More</a>
+          <a href="/" className="btn btn-sm btn-primary">ReadMore</a>
         </div>
       </div>
     )
