6a2bf6ddad33ec6cb0d671f61df6a73c173eaa54
bhanu1999 <sakinalabhanu99@gmail.com>
2022-05-23 18:54:01 +0530

    fixing navbar and also fixing card sizes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,25 +3,21 @@ import React, { Component } from 'react'
 export class NavBar extends Component {
   render() {
     return (
-         <nav className="navbar navbar-dark bg-dark navbar-expand-lg " style={{height:"80px"}}>
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/"><img style={{height:"80px"}}src="capture.png" alt='img not found'></img></a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/" style={{size:"80px", padding:"5px"}}><h3><b>HOME</b></h3></a>
-        </li>
-       
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark" style={{height:"60px",background:"#18182c"}}>
+      <a className="navbar-brand" href="/"><img style={{height:"60px",width:"150px"}} src='capture.png' alt='logo replacement'></img></a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+    
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="/home">Home </a>
+          </li>
+          </ul>
         
-        
-      </ul>
-      
-    </div>
-  </div>
-</nav>
+      </div>
+    </nav>
     )
   }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -4,12 +4,12 @@ export class NewsItem extends Component {
   render() {
      let {title,description,imageUrl,newsUrl}=this.props;
     return (
-        <div className="card my-3" style={{width: "18rem"}} >
+        <div className="card my-3" style={{width: "18rem",height:"550px"}} >
         <img src={imageUrl} style={{height:"180px"}} className="card-img-top" alt="Img Not Found"/>
         <div className="card-body">
           <h5 className="card-title">{title}...</h5>
           <p className="card-text">{description}...</p>
-          <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary">ReadMore</a>
+          <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">ReadMore</a>
         </div>
       </div>
     )
